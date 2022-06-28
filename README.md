4f53334771c63ab75582bbe05544c24cca544929
Jhenifer <jhenifer.msantos@gmail.com>
2022-06-27 21:11:08 -0300

    Aula 2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# alura_loja_go
+# Loja utilizando Go
 Estudo de Go com base no curso do alura
+
+Aula 2:
+precisar estar na estrutura de pastas github.com/your_user/project
+Execute:
+    go env -w GO111MODULE=auto
+    go get github.com/lib/pq, 
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=1234 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -20,12 +35,33 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
+	db := conectaComBancoDeDados()
+
+	produtosFromDatabase, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtos := []Produto{}
+	for produtosFromDatabase.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = produtosFromDatabase.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produto := Produto{id, nome, descricao, preco, quantidade}
+
+		produtos = append(produtos, produto)
+
 	}
 
 	templates.ExecuteTemplate(w, "Index", produtos)
+
+	defer db.Close()
 }
